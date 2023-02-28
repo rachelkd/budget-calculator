f240a7beead31d806a74be7654abc11cc8aa49f2
orachel <rachel05deng@gmail.com>
2023-02-28 13:39:24 -0800

    Example added

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import NavBar from './NavBar.svelte';
 	import Title from './Title.svelte';
+	import Example from './Example.svelte';
 </script>
 
 <NavBar />
 <Title title = "Add expense"/>
 <Title title = "Expense list"/>
+<Example />
