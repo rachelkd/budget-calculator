64d66c77db1a38440a26140d12916e6b6d6afc5f
orachel <rachel05deng@gmail.com>
2023-02-28 11:32:50 -0800

    First commit

---
+++ src/NavBar.svelte
@@ -0,0 +1,9 @@
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">Budget Calculator</h1>
+		<button type="button" class="nav-btn">
+			<i class="far fa-plus-square"></i>
+			Add item
+		</button>
+	</div>
+</nav>
