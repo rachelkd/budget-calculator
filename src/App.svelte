7bb42ad8fb6c61c13d031c594e59a8c44272bfb0
orachel <rachel05deng@gmail.com>
2023-03-04 01:37:55 -0800

    totals, form creates

--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,24 @@
 	// Components
 	import NavBar from "./NavBar.svelte";
 	import ExpensesList from "./ExpensesList.svelte";
+	import Totals from "./Totals.svelte";
+	import ExpenseForm from "./ExpenseForm.svelte";
 	// Data
 	import expensesData from "./expenses";
 	// Variables
 	let expenses = [...expensesData];
+	// Reactive
+	$: total = expenses.reduce((acc,curr)=>{
+		// console.log({acc,amount:curr.amount});
+		return (acc += curr.amount)
+	},0);
 	console.log(expenses);
 	// Functions
 	function removeExpense(id){
 		expenses = expenses.filter(item => item.id !== id);
+	}
+	function clearExpenses() {
+		expenses = [];
 	}
 	// Context
 	setContext("remove", removeExpense);
@@ -18,5 +28,10 @@
 
 <NavBar />
 <main class="content">
+	<ExpenseForm />
+	<Totals title = "Total expenses" total = {total}/>
 	<ExpensesList {expenses} />
+	<button type="button" class = "btn btn-primary btn-block" on:click={clearExpenses}>
+		Clear expenses
+	</button>
 </main>
