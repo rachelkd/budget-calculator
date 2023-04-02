cc3bdd274b744defc1f80dbb7cc55dd01bb0f551
orachel <rachel05deng@gmail.com>
2023-04-02 13:16:08 -0700

    animate

--- src/ExpensesList.svelte
+++ src/ExpensesList.svelte
@@ -2,6 +2,7 @@
     import SectionTitle from "./Title.svelte";
     import Expense from "./Expense.svelte";
     import { fly } from "svelte/transition";
+    import { flip  } from "svelte/animate";
     export let expenses = [];
 </script>
 
@@ -14,8 +15,8 @@
 <section>
     <SectionTitle title="Expense list"/>
     <ul>
-		{#each expenses as expense, index}
-            <div transition:fly = {{ x: 200, delay: index*308 }}>
+		{#each expenses as expense, index (expense.id)}
+            <div in:fly = {{ x: 200, delay: index*308 }} out:fly = {{ x: -200, delay: index*308 }} animate:flip >
                 <Expense {...expense} />
             </div>
             {:else}
